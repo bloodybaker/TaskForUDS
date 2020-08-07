ca4184af75c0885691e5f2c24844ebdc38cdfbdb
Yevhenii Chykalov <gamchanel@gmail.com>
2020-08-07 09:48:31 +0300

    Fixed TestNoWeekEnd

--- TestTaksProject/WorkDayCalculator.cs
+++ TestTaksProject/WorkDayCalculator.cs
@@ -8,48 +8,62 @@ using System.Collections.Generic;
         private DateTime result;
         private List<DateTime> dateTimes = new List<DateTime>();
         private List<DateTime> exeptDates =  new List<DateTime>();
+
         public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
         {
-            foreach (var date in weekEnds)
+            if (weekEnds != null)
             {
-                for (int i = 0; i <= (date.EndDate-date.StartDate).TotalDays; i++)
+                foreach (var date in weekEnds)
                 {
-                    exeptDates.Add(date.StartDate.AddDays(i));
+                    for (int i = 0; i <= (date.EndDate - date.StartDate).TotalDays; i++)
+                    {
+                        exeptDates.Add(date.StartDate.AddDays(i));
+                    }
                 }
-            }
 
-            foreach (var date in exeptDates)
-            {
-                Console.WriteLine(date.ToShortDateString());
-            }
+                foreach (var date in exeptDates)
+                {
+                    Console.WriteLine(date.ToShortDateString());
+                }
 
-            Console.WriteLine("==========================");
-            for (int i = 0; i < dayCount; i++)
-            {
-                dateTimes.Add(startDate.AddDays(i));
-            }
+                Console.WriteLine("==========================");
+                for (int i = 0; i < dayCount; i++)
+                {
+                    dateTimes.Add(startDate.AddDays(i));
+                }
 
-            int countOfWeekends = exeptDates.Count;
-            DateTime lastday = dateTimes[dateTimes.Count - 1];
-            for (int i = 1; i < countOfWeekends; i++)
-            {
-                dateTimes.Add(lastday.AddDays(i));
-            }
-            foreach (var exceptDate in exeptDates)
-            {
+                int countOfWeekends = exeptDates.Count;
+                DateTime lastday = dateTimes[dateTimes.Count - 1];
+                for (int i = 1; i < countOfWeekends; i++)
+                {
+                    dateTimes.Add(lastday.AddDays(i));
+                }
+
+                foreach (var exceptDate in exeptDates)
+                {
                     Console.WriteLine("Выходной: = " + exceptDate.ToShortDateString());
                     dateTimes.Remove(exceptDate);
-            }
+                }
 
-            Console.WriteLine("Колво-выходных " + countOfWeekends);
-            Console.WriteLine("==========================");
-            
-            foreach (var element in dateTimes)
-            {
-                Console.WriteLine("Рабочий день: " + element.ToShortDateString());
+                Console.WriteLine("Колво-выходных " + countOfWeekends);
+                Console.WriteLine("==========================");
+
+                foreach (var element in dateTimes)
+                {
+                    Console.WriteLine("Рабочий день: " + element.ToShortDateString());
+                }
+
+                return dateTimes[dateTimes.Count - 1];
             }
-            
-            return dateTimes[dateTimes.Count-1];
+            else
+            {
+                for (int i = 0; i < dayCount; i++)
+                {
+                    dateTimes.Add(startDate.AddDays(i));
+                }
+
+                return dateTimes[dateTimes.Count - 1];
+            }
         }
     }
 }
